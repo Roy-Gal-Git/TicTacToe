db7db663305e65d05bac225490651a0d254478b6
Roy-Gal-Git <Self.roygal@gmail.com>
2021-04-12 11:53:11 +0300

    master

---
+++ game.py
@@ -0,0 +1,26 @@
+positions = {}
+for p in range(1, 10):
+    positions[p] = str(p)
+
+board = '   |   |   \n {} | {} | {} \n   |   |   \n-----------\n   |   |   \n {} | {} | {} \n   |   |   \n-----------\n   |   |   \n {} | {} | {} \n   |   |   '
+
+
+def print_board(counter=1):
+    print(board.format(positions[1], positions[2], positions[3], positions[4], positions[5], positions[6], positions[7],
+                       positions[8], positions[9]))
+    if counter > 9:
+        print("WINNER!")
+        pass
+    else:
+        if counter % 2 == 0:
+            o_place = int(input("O's Turn!\nPick a position: "))
+            positions[o_place] = "O"
+            counter += 1
+        else:
+            x_place = int(input("X's Turn!\nPick a position: "))
+            positions[x_place] = "X"
+            counter += 1
+        return print_board(counter)
+
+
+print_board()
