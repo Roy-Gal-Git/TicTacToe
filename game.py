2ba8137270844638d48377d2818b7681312960b2
Roy-Gal-Git <Self.roygal@gmail.com>
2021-10-14 15:41:08 +0300

    Added error handling for inputting strings and numbers that are not between 1-9

--- game.py
+++ game.py
@@ -6,23 +6,44 @@ for p in range(1, 10):
 board = '   |   |   \n {} | {} | {} \n   |   |   \n-----------\n   |   |   \n {} | {} | {} \n   |   |   \n-----------\n   |   |   \n {} | {} | {} \n   |   |   '
 
 
+class NotInRange(Exception):
+    pass
+
+
 def mapBoard():
     print(board.format(positions[1], positions[2], positions[3], positions[4], positions[5], positions[6], positions[7],
                        positions[8], positions[9]))
 
 
-def print_board(counter=1):
+def get_input(player):
+    while True:
+        try:
+            user_input = int(input(player + "'s turn!\nPick a position: "))
+
+            if user_input < 1 or user_input > 9:
+                raise NotInRange
+
+            return user_input
+
+        except ValueError as e:
+            print("Please enter a number between 1-9!\n")
+
+        except NotInRange:
+            print("Please enter a number between 1-9!\n")
+
+
+def print_map(counter=1):
     mapBoard()
     if counter > 9:
         print("WINNER!")
         pass
     else:
         if counter % 2 == 0:
-            o_place = int(input("O's Turn!\nPick a position: "))
+            o_place = get_input("O")
             positions[o_place] = "O"
             counter += 1
         else:
-            x_place = int(input("X's Turn!\nPick a position: "))
+            x_place = get_input("X")
             positions[x_place] = "X"
             counter += 1
     if algo.checkWinner(positions) == 'X':
@@ -32,7 +53,14 @@ def print_board(counter=1):
         mapBoard()
         print("O WINS!")
     else:
-        return print_board(counter)
+        return print_map(counter)
 
 
-print_board()
+def exit_func():
+    print("Press ctrl + c to exit: ")
+    while True:
+        pass
+
+
+print_map()
+exit_func()
